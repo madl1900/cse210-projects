43f03268a33002296a3142321cdba150c9598410
madl1900 <lou24011@byui.edu>
2026-01-13 18:41:12 -0700

    Did Prep 2, using if and else statements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string stringPercent =Console.ReadLine();
+        int intPercent = int.Parse(stringPercent);
+
+        // define variable letterGrade
+        string letterGrade = "";
+
+        // check what the percentage is and update letterGrade variable
+        if (intPercent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (intPercent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (intPercent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (intPercent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letterGrade}");
+
+        // check if they passed or failed the class
+        if (intPercent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class but if you study hard you will do better next time!");
+        }
     }
 }
