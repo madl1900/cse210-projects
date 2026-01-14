0021b06c7887cfb38743a19de0ea606b61b47ab5
madl1900 <lou24011@byui.edu>
2026-01-13 17:22:53 -0700

    added notes for week 2

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        
+        // You must declare variable types
+        int x = 5;
+        Console.WriteLine(x);
+
+        // put inputs on a separate line
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+
     }
 }
+
+// WEEK 2 NOTES - JAN 13, 2026
+
+// comment line
+
+/* comment over
+multiple lines */
+
+// naming in C# for public files is ThisIsTheName.cs, private files are tHISiStHEnAME.cs
+ 
