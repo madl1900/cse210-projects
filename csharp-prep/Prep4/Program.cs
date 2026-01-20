a43b75a7c0add86b55ad2be71f33092a7ac94613
madl1900 <lou24011@byui.edu>
2026-01-20 11:09:42 -0700

    made a program that adds numbers to a list, to practice lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNum = -1;
+
+        while (userNum != 0)
+        {
+            Console.Write("What number would you like to add to the list? ");
+            string userNumStr = Console.ReadLine();
+            userNum = int.Parse(userNumStr);
+
+            if (userNum != 0)
+            { 
+                numbers.Add(userNum);
+            }
+        }
+        int sum = 0;
+        int max = 0;
+        foreach (int num in numbers)
+        {
+            sum = sum + num;
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        int average = sum/numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
