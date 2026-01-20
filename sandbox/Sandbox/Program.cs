3b4acac120dbd11dc3a6552a011d91368466f443
madl1900 <lou24011@byui.edu>
2026-01-20 14:51:52 -0700

    added notes for week 3

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,24 +4,123 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
         
-        // WEEK 2 NOTES - JAN 13, 2026
+        // // WEEK 2 NOTES - JAN 13, 2026
         
-        // comment line
-        /* comment over
-        multiple lines */
+        // // comment line
+        // /* comment over
+        // multiple lines */
         
-        // naming in C# for public files is ThisIsTheName.cs, private files are tHISiStHEnAME.cs
+        // // naming in C# for public files is ThisIsTheName.cs, private files are tHISiStHEnAME.cs
         
-        // You must declare variable types
-        int x = 5;
-        Console.WriteLine(x);
+        // // You must declare variable types
+        // int x = 5;
+        // Console.WriteLine(x);
 
-        // put inputs on a separate line
-        Console.Write("What is your favorite color? ");
-        string color = Console.ReadLine();
+        // // put inputs on a separate line
+        // Console.Write("What is your favorite color? ");
+        // string color = Console.ReadLine();
+
+
+        // // WEEK 3 NOTES - Jan 20, 2026
+
+        // // if statements
+        // int number = 12;
+        // if (number == 0)
+        //     Console.WriteLine("Your number is zero");
+        // else if (number < 10 && number > -10)
+        //     {
+        //         Console.WriteLine($"Your number ({number}) is single digit");
+        //     }
+        // else if (number <= 10 || number >= -10)
+        //     {
+        //         Console.WriteLine($"Your number ({number}) is multi-digit");
+        //     }
+        // // {} for if and loops statements are only necessary if the code is more than one line
+
+        // // while loops
+        // string response = "yes";
         
+        // while (response == "yes")
+        // {
+        //     Console.Write("Do you want to continue? ");
+        //     response = Console.ReadLine(); 
+        // }
+
+        // Console.WriteLine("Done!");
+
+        // // do while loops
+
+        // string response2;
+        // do
+        // {
+        //     Console.Write("Do you want to continue? ");
+        //     response2 = Console.ReadLine(); 
+        // } 
+        // while (response2 == "yes"); 
+
+        // Console.WriteLine("Done!");
+
+
+        // // foreach loops
+
+        // List<string> words = new List<string>();
+        // words.Add("cold");
+        // words.Add("snow");
+        // words.Add("soccer");
+
+        // foreach (string word in words)
+        // {
+        //     Console.WriteLine(word);
+        // }
+        
+
+        // // for loops
+
+        // for(int i = 0; i < 10; i++) // 1.declare variable 2.condition 3.what to do with variable at end
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        // for(int i = 10; i > 0; i--)
+        // {
+        //    Console.WriteLine(i);
+        // }
+
+        // List<string> words2 = new List<string>();
+        // words2.Add("cold");
+        // words2.Add("snow");
+        // words2.Add("soccer");
+
+        // for (int i = 0; i < words2.Count; i++)
+        // {
+        //     Console.WriteLine(words2[i]);
+        // }
+
+        // for (int i = words2.Count - 1; i >= 0; i--)
+        // {
+        //     Console.WriteLine(words2[i]);
+        // }
+
+        // functions
+
+        // void DisplayMessage(string name)
+        // {
+        //     Console.WriteLine($"Hello {name}!");
+        // }
+
+        // DisplayMessage("World");
+        // DisplayMessage("Madison");
+
+        // int Add(int a, int b)
+        // {
+        //     return a + b;
+        // }
+
+        // Console.WriteLine(Add(1,2));
+
+
 
     }
 }
