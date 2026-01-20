e78a5648f3c264748acc6e5a5ef9b28642d8661f
madl1900 <lou24011@byui.edu>
2026-01-20 10:45:51 -0700

    Created a number guessing game with random generator to practice loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the magic number generator!");
+        Console.WriteLine("Where you try to guess the magic number!");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 100);
+        
+        int guessInt = 101;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guessStr = Console.ReadLine();
+            guessInt = int.Parse(guessStr);
+
+            if (guessInt > magicNum)
+            {
+            Console.WriteLine("Lower");   
+            }
+            else if (guessInt < magicNum)
+            {
+            Console.WriteLine("Higher");   
+            }
+            else
+            {
+            Console.WriteLine("You guessed it!");   
+            }
+        } while (guessInt != magicNum);
     }
 }
