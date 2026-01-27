1a1170d3593f75f6b0a7e7f014884b61d7272e93
madl1900 <lou24011@byui.edu>
2026-01-26 17:11:51 -0700

    practiced creating classes and using them in a program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "The Piano Place";
+        job1._jobTitle = "Piano Teacher";
+        job1._startYear = "2021";
+        job1._endYear = "current";
+
+
+        Job job2 = new Job();
+        job2._company = "The Megaplex";
+        job2._jobTitle = "Concessions Team Member";
+        job2._startYear = "2018";
+        job2._endYear = "2019";
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Madison Loutensock";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
