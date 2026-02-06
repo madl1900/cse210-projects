0a95e9c1e5cad592adec095b55c2f07ae3bdad5e
madl1900 <lou24011@byui.edu>
2026-02-05 19:42:03 -0700

    added comments to explain what I did to exceed requirements

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,5 +1,11 @@
 using System;
-
+/* I exceeded the requirements for this assignment by 
+adding a feature in the DisplayJournal method of the 
+Journal class that asks the user whether they want to
+display all entries or only the ones from a certain date.
+if they choose date, it then uses an if statement to only
+display the entries that match the chosen date.
+*/
 class Program
 {
     static void Main(string[] args)
