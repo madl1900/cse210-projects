09c1faa1087c957916b4aa1bbec33d488d8cfdf1
madl1900 <lou24011@byui.edu>
2026-03-10 20:39:42 -0600

    finished prep activity for polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square mySquare = new Square("blue", 12);
+
+        Rectangle myRectangle = new Rectangle("red", 10, 3);
+
+        Circle myCircle = new Circle("yellow", 5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(mySquare);
+        shapes.Add(myRectangle);
+        shapes.Add(myCircle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()} Area: {shape.GetArea()}");
+        }
     }
 }
